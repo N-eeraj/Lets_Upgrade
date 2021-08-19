3c0bdb778dd16472c5ed7db8b7327639d8154a75
N-eeraj <neeraj7rajeena@gmail.com>
2021-08-19 22:35:56 +0530

    scraped quotes

--- Web Scraping Aug 2021/quotes/quotes/spiders/quotes_spider.py
+++ Web Scraping Aug 2021/quotes/quotes/spiders/quotes_spider.py
@@ -5,5 +5,8 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com/"]
 
     def parse(self, response, **kwargs):
-        title = response.xpath("//title/text()").extract()[0]
-        return f"Title {title}"
+        # title = response.xpath("//title/text()").extract()[0]
+        # yield {"Title": title}
+
+        quotes = response.css(".text::text").extract()
+        yield {"Quotes": quotes}
