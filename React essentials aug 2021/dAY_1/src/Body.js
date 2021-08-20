83dd2322095ac759404d439e45aa6ebef525866b
N-eeraj <neeraj7rajeena@gmail.com>
2021-08-20 16:57:55 +0530

    React day 1 Assignment

---
+++ React essentials aug 2021/dAY_1/src/Body.js
@@ -0,0 +1,37 @@
+function Body()
+{
+    return(
+        <div>
+            <hr/>
+            <h3>This is the 2nd Component</h3>
+            <p>
+                This component has the body section of this SPA(Single Page Application)
+                <hr/>
+                <br/>
+                <strong>This is also 2nd Component, but it has some dummy text</strong>
+                <br/>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada tincidunt 
+                    ipsum, sed mattis ex commodo ac. Praesent id felis a eros pulvinar pellentesque. Nunc 
+                    laoreet orci ut eleifend molestie. Donec pulvinar ultricies elit ut eleifend. Aliquam 
+                    erat volutpat. Donec ligula lacus, pulvinar quis magna at, tempus dignissim eros. Sed 
+                    tristique dui vel eros ullamcorper, non ullamcorper augue sollicitudin. Quisque sit amet 
+                    aliquet augue. Vestibulum eleifend ex id neque feugiat, a mollis tortor ultricies. Nam 
+                    efficitur odio libero, non viverra diam posuere ac. Sed sed commodo nisl.
+                </p>
+                <p>
+                    Pellentesque consequat velit vitae lorem hendrerit ultricies. Vestibulum ante ipsum 
+                    primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas at erat 
+                    et quam mollis condimentum eu sit amet dui. Nulla facilisi. Nulla venenatis porttitor 
+                    mi, nec malesuada felis aliquam vulputate. Donec auctor interdum viverra. Class aptent 
+                    taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum 
+                    eu purus tristique, dapibus purus et, pellentesque ligula. Mauris ut odio sed leo venenatis 
+                    tempor id id enim. Suspendisse potenti. Donec in elit metus. Vestibulum elementum elit ac 
+                    elit ornare, at lacinia risus fermentum. Maecenas et elementum lorem.
+                </p>
+            </p>
+        </div>
+    )
+}
+
+export default Body;
---
+++ React essentials aug 2021/dAY_1/src/Header.js
@@ -0,0 +1,11 @@
+function Header()
+{
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <h2>This is my 1st component</h2>
+        </div>
+    );
+}
+
+export default Header;
